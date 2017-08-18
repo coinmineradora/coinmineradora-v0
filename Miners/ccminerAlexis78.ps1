8319d7116a21ee6b28874482a0944d38150d50e6
coinmineradora <31132993+coinmineradora@users.noreply.github.com>
2017-08-18 17:01:53 -0300

    Add files via upload

---
+++ Miners/ccminerAlexis78.ps1
@@ -0,0 +1,53 @@
+. .\Include.ps1
+
+$Path = ".\Bin\NVIDIA-Alexis78\ccminer.exe"
+$Uri = "https://github.com/nemosminer/ccminer-Alexis78/releases/download/ccminer-alexis78/ccminer-alexis78-ms2013-cuda7.5.7z"
+
+$Commands = [PSCustomObject]@{
+    #"bitcore" = "" #Bitcore
+    "blake2s" = "" #Blake2s
+    "blakecoin" = "" #Blakecoin
+    #"vanilla" = "" #BlakeVanilla
+    #"cryptonight" = "" #Cryptonight
+    #"decred" = "" #Decred
+    #"equihash" = "" #Equihash
+    #"ethash" = "" #Ethash
+    #"groestl" = "" #Groestl
+    #"hmq1725" = "" #hmq1725
+    "keccak" = " -i 24" #Keccak
+    "lbry" = "" #Lbry
+    "lyra2v2" = "" #Lyra2RE2
+    #"lyra2z" = "" #Lyra2z
+    "myr-gr" = "" #MyriadGroestl
+    #"neoscrypt" = " -i 15" #NeoScrypt
+    "nist5" = "" #Nist5
+    #"pascal" = "" #Pascal
+    "Quark" = "" #Quark
+    "qubit" = "" #Qubit
+    #"scrypt" = "" #Scrypt
+    #"sia" = "" #Sia
+    "sib" = " -i 21" #Sib
+    "skein" = "" #Skein
+    "veltor" = "" #veltor
+    #"timetravel" = "" #Timetravel
+    "c11" = " -i 21" #C11
+    #"x11evo" = "" #X11evo
+    "x17" = " -i 21" #X17
+    "x11" = "" #X11
+    #"yescrypt" = "" #Yescrypt
+}
+
+$Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
+
+$Commands | Get-Member -MemberType NoteProperty | Select -ExpandProperty Name | ForEach {
+    [PSCustomObject]@{
+        Type = "NVIDIA"
+        Path = $Path
+        Arguments = "-a $_ -o stratum+tcp://$($Pools.(Get-Algorithm($_)).Host):$($Pools.(Get-Algorithm($_)).Port) -u $($Pools.(Get-Algorithm($_)).User) -p $($Pools.(Get-Algorithm($_)).Pass)$($Commands.$_)"
+        HashRates = [PSCustomObject]@{(Get-Algorithm($_)) = $Stats."$($Name)_$(Get-Algorithm($_))_HashRate".Week}
+        API = "Ccminer"
+        Port = 4068
+        Wrap = $false
+        URI = $Uri
+    }
+}
